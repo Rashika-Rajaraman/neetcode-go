c95ec93b8ed77af9bde44dc882878d0dd3347b80
Rashika <rashika.rajaraman@intel.com>
2026-06-23 11:45:50 +0530

    Create submission-0.go

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+   
+   // Brute Force
+   // Time Complexity - O(n2)
+   // Space Complexity - O(1)
+    for i:=0; i<len(nums);i++{
+      for j:=i+1; j<len(nums);j++{
+         if nums[i]+nums[j] == target {
+           return []int{i, j}
+         }
+      }
+    }
+    return []int{}
+}
